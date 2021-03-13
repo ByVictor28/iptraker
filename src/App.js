164c97c5e3d07896aa2ee13aaff17d59647a38e0
victor manuel delfin santos <vdelfin.yunius@gmail.com>
2021-03-13 16:51:01 -0600

    Proyect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import classes from './App.module.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import axios from "axios"
+import Spiner from "./Spiner/Spiner";
 
 function App() {
+
+  const [reqData, setReqData] = useState({})
+  const [coords, setcoords] = useState(null)
+  const [ipAddress, setIpAddress] = useState("")
+
+  useEffect(() => {
+    axios.get("https://api-nod.herokuapp.com/ip")
+    .then((result) => {
+      setReqData(result.data.res)
+      setcoords(result.data.res.coords)
+      console.log(result)
+    }).catch((err) => {
+      console.log(err)
+    });
+  }, [])
+
+  const SearchIp = () => {
+    setcoords(null)
+    axios.get("https://api-nod.herokuapp.com/ip?q="+ipAddress)
+    .then((result) => {
+      if(!result.data.error){
+        setReqData(result.data.res)
+        setcoords(result.data.res.coords)
+      }else{
+        setReqData({
+          ip:"NOT FOUND",
+          location:"NOT FOUND",
+          timezone:"NOT FOUND",
+          ISP:"NOT FOUND"
+        })
+      }
+      
+    }).catch((err) => {
+      console.log(err)
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.Header} style={{backgroundImage:"url(/images/pattern-bg.png)"}}>
+        <h1>IP Address Tracker </h1>
+        <div className={classes.Search}>
+          <input type="text" placeholder="Search for any API address or domain" value={ipAddress} onChange={(e) => setIpAddress(e.target.value)}/>
+          <input type="submit" value=">" onClick={SearchIp}/>
+        </div>
+        <div className={classes.Details}> 
+          <div>
+            <h2>IP ADDRESS</h2>
+            <p>{reqData.ip?reqData.ip:"NOT FOUND"}</p>
+          </div>
+          <div>
+            <h2>LOCATION</h2>
+            <p>{reqData.location?reqData.location:"NOT FOUND"}</p>
+          </div>
+          <div>
+            <h2>TIMEZONE</h2>
+            <p>{reqData.timezone?reqData.timezone:"NOT FOUND"}</p>
+          </div>
+          <div>
+            <h2>ISP</h2>
+            <p>{reqData.ISP?reqData.ISP:"NOT FOUND"}</p>
+          </div>
+        </div>     
+      </div>
+      <div className={classes.Map}>
+        {
+          coords===null?
+          <Spiner></Spiner>
+          : 
+          <MapContainer className={classes.MapContainer} center={[coords.lat, coords.lon]} zoom={6} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[coords.lat, coords.lon]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Spiner/Spiner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from "./Spiner.module.css"
+
+const Spiner = ()=>{ 
+return (
+    <div className={classes.loader}>
+        Loading...
+    </div>
+) 
+}
+
+export default Spiner
